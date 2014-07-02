da2606641bf38958f14ddb6a7c9e30254b0e48ad
NPM <npm@nuestrared.com>
2014-07-02 16:37:54 +0200

    commit jobs folder

--- vagrant/puppet/modules/php/manifests/init.pp
+++ vagrant/puppet/modules/php/manifests/init.pp
@@ -1,7 +1,7 @@
 # vagrant/puppet/modules/php/manifests/init.pp
 class php {
   # Install the php5-fpm and php5-cli packages
-  package { ['php5-fpm', 'php5-cli', 'php5-mysql', 'php5-xdebug']:
+  package { ['php5-fpm', 'php5-cli', 'php5-mysql', 'php5-xdebug', 'php5-curl']:
     ensure => present,
     require => Exec['apt-get update'],
     notify => Service['php5-fpm']
