e3eb11ccaf865ea23cd92f6a08702dd6a1592cbb
npoggi <npmnpm@gmail.com>
2014-07-14 15:55:53 +0200

    adding max exe time filter

--- aloja-web/inc/common.php
+++ aloja-web/inc/common.php
@@ -81,7 +81,8 @@ if (in_dev()) echo "<!--NO CACHE: $sql --->\n";
     return $rows;
 }
 
-$filter_execs = "AND exe_time between 200 AND 20000 AND (id_cluster = 1 OR (bench != 'bayes' AND id_cluster=2))";
+$filter_execs = "AND exe_time > 200 AND (id_cluster = 1 OR (bench != 'bayes' AND id_cluster=2))";
+$filter_execs_max_time = "AND exe_time < 10000";
 function get_execs() {
     global $filter_execs;
     $query = "SELECT e.*, (exe_time/3600)*(cost_hour) cost  FROM execs e
@@ -449,7 +450,7 @@ function make_header ($title = 'HiBench Executions on Hadoop', $message = null) 
       <div class="modal-body">
         <p><strong>ALOJA</strong> is a project to explore Hadoop\'s performance under different Software parameters, Hardware, Cloud or On-Premise, and Job types.
             This site is under constant development and in the process of being documented.
-            For inquiries, feature requests or bug reports please contact us at: <a href="mailto:aloja@bsc.es" target="_top">aloja@bsc.es</a></p>
+            For inquiries, feature requests or bug reports please contact us at: <a href="mailto:hadoop@bsc.es" target="_top">hadoop@bsc.es</a></p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
