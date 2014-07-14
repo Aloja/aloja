ea5edb58601706bb0593222f681183c75248f56a
Aaron <aaron.call@bsc.es>
2014-07-14 15:48:39 +0200

    Welcome page

--- aloja-web/index.php
+++ aloja-web/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once('datatable.php');
+require_once('inc/common.php');
+
+echo $twig->render('welcome.html.twig', array(
+		 'selected' => 'Welcome',
+		 'show_in_result' => null,
+		 'table_fields' => null,
+		 'message' => $message
+		));
