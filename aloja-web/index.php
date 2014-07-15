3f62d509a7ea3027b4b501c099403c1e5a10f720
Aaron <aaron.call@bsc.es>
2014-07-15 10:16:25 +0200

    fixed mail and about page selection errors

--- aloja-web/index.php
+++ aloja-web/index.php
@@ -3,7 +3,7 @@
 require_once('inc/common.php');
 
 echo $twig->render('welcome.html.twig', array(
-		 'selected' => 'Welcome',
+		 'selected' => 'About',
 		 'show_in_result' => null,
 		 'table_fields' => null,
 		 'message' => $message
