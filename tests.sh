b294ed11b4f2aa7c3b0c58cb08c26dc8dc65a066
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-04 19:52:36 +0200

    Using built-in php server

--- tests.sh
+++ tests.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-npm test
+base_url="http://localhost:8080"
+echo "does it work?"
+result=$(curl --silent "$base_url/aloja-web/serverWorksTest.php")
+
+if [ "$result" = "hello" ]
+then
+   npm test
+   exit 0
+fi
+
+echo "failed :("
+exit 1
