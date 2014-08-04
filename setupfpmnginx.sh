4be906b38a298d1c41f6de023238ede2b4dbf434
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-04 19:37:09 +0200

    testing server started correctly

--- setupfpmnginx.sh
+++ setupfpmnginx.sh
@@ -2,15 +2,11 @@
 sudo cp .travis_nginx.conf /etc/nginx/nginx.conf
 sudo service nginx restart
 
-base_url="http://localhost:8080`pwd`"
+base_url="http://localhost:8080"
 echo "does it work?"
-echo "raw:"
-curl --silent "$base_url/about.php"
-result=`curl --silent "$base_url/about.php"`
+result=$(curl --silent "$base_url/aloja-web/serverWorksTest.php")
 
-echo "result:"
-
-if [ "$result" == "hello" ]
+if [ "$result" = "hello" ]
 then
     echo "works!"
     exit 0;
@@ -18,3 +14,4 @@ fi
 
 echo "failed :("
 exit 1
+
