58431ff04b764ff766879aae1f11019b4dce8a78
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-04 18:12:05 +0200

    travis CI test

--- aloja-web/inc/config.sample.php
+++ aloja-web/inc/config.sample.php
@@ -6,4 +6,4 @@
     //Prod MySQL, need to SSH first in Vagrant VM with: ssh -L localhost:3307:gallactica:3306 user@minerva.bsc.es
     //define('DB_CONN_CHAIN','mysql:host=127.0.0.1;dbname=aloja2;port=3307');
     define('IN_CACHE',false);
-    define('ENABLE_DEBUG',true);
+    define('ENABLE_DEBUG',false);
