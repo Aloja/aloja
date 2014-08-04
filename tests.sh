12db425c71f9cb3661fa736560782505a62eb478
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-04 20:09:00 +0200

    Caching exit code status of npm tests

--- tests.sh
+++ tests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 base_url="http://localhost:8080"
-echo "does it work?"
 result=$(curl --silent "$base_url/aloja-web/serverWorksTest.php")
 
 if [ "$result" = "hello" ]
 then
+   echo "Server is working\nRunning functional tests.."
    npm test
-   exit 0
+   ret_code=$?
+   echo "exiting with ret code $ret_code\n"
+   exit $ret_code
+else
+  echo "Server is not working"
+  exit 1
 fi
-
-echo "failed :("
-exit 1
