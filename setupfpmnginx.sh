04ce85bda6a4c98de3dae11d720adbdcd3fc3202
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-04 19:16:58 +0200

    Checking nginx is running

--- setupfpmnginx.sh
+++ setupfpmnginx.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 sudo cp .travis_nginx.conf /etc/nginx/nginx.conf
 sudo service nginx restart
+
+base_url="http://localhost:8080`pwd`"
+echo "does it work?"
+echo "raw:"
+curl --silent "$base_url/about.php"
+result=`curl --silent "$base_url/about.php"`
+
+echo "result:"
+
+if [ "$result" == "hello" ]
+then
+    echo "works!"
+    exit 0;
+fi
+
+echo "failed :("
+exit 1
