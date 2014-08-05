6a7be5fba00a593e697de68a6cd2f65768f07007
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-05 10:28:12 +0200

    Improved travis CI tests script

--- tests.sh
+++ tests.sh
@@ -4,12 +4,12 @@ result=$(curl --silent "$base_url/aloja-web/serverWorksTest.php")
 
 if [ "$result" = "hello" ]
 then
-   echo "Server is working\nRunning functional tests.."
+   echo -e "\e[1;42mPASS Server is working\e[0m\nRunning functional tests..\n"
    npm test
    ret_code=$?
    echo "exiting with ret code $ret_code\n"
    exit $ret_code
 else
-  echo "Server is not working"
+  echo -e "\e[1;101mFAIL Server is NOT working\e[0m\n"
   exit 1
 fi
