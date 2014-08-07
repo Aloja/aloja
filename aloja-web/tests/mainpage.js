214c59ed2d2ffe43734b929ecb956f326d01bcad
MortI2C <aaroncallbarreiro@gmail.com>
2014-08-07 20:31:30 +0200

    deleting config file

--- aloja-web/tests/mainpage.js
+++ aloja-web/tests/mainpage.js
@@ -1,6 +1,6 @@
 casper.test.begin("Mainpage tests", function(test) {
    casper.start('http://localhost:8080/', function() {
-     test.assertTitle('ALOJA, BSC\'s Hadoop Benchmark Repository and Online Performance Analysis Tools', 'title is correct');
+     test.assertTitle('ALOJA, BSC\'s Hadoop Benchmark Repository and Online Performance Analysis Tools', 'Title is correct');
      test.assertExists('.popup-youtube', 'Video demo exists');
    });
 
