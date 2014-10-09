d8c366940631d3ff1d17ac710b95d5c603f58eb0
Josep Cugat <jcugat@gmail.com>
2014-10-09 17:29:52 +0200

    Added Point comparison

--- aloja-web/inc/dbscan/Point.php
+++ aloja-web/inc/dbscan/Point.php
@@ -16,6 +16,29 @@ class Point
         $this->info = $info;
     }
 
+    /**
+     * Compares the current object to the passed $other.
+     *
+     * Returns 0 if they are semantically equal, 1 if the other object is less
+     * than the current one, or -1 if its more than the current one.
+     */
+    public function compareTo($other)
+    {
+        if ($this->x > $other->x) {
+            return 1;
+        } else if ($this->x < $other->x) {
+            return -1;
+        } else {
+            if ($this->y > $other->y) {
+                return 1;
+            } else if ($this->y < $other->y) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
     public function __toString()
     {
         return "(".$this->x.",".$this->y.")";
