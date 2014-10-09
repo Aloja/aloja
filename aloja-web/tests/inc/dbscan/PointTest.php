9147afd6665f5519bc0cf16cdd15b81a9328343f
Josep Cugat <jcugat@gmail.com>
2014-10-09 17:29:52 +0200

    Added Point comparison

--- aloja-web/tests/inc/dbscan/PointTest.php
+++ aloja-web/tests/inc/dbscan/PointTest.php
@@ -48,4 +48,52 @@ class PointTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(in_array($p1, $data, $strict = true));
         $this->assertTrue(in_array($p2, $data, $strict = true));
     }
+
+    /**
+     * @dataProvider compareToProvider
+     */
+    public function testCompareTo($p1, $p2, $compare_result)
+    {
+        $this->assertEquals($compare_result, $p1->compareTo($p2));
+    }
+
+    public function compareToProvider()
+    {
+        // Array content:
+        // point 1, point 2, compare result
+        return array(
+            array(new Point(0, 0), new Point(0, 0), 0),
+            array(new Point(15, 15), new Point(15, 15), 0),
+            array(new Point(14, 16), new Point(14, 16), 0),
+
+            array(new Point(15, 15), new Point(15, 14), 1),
+            array(new Point(15, 15), new Point(15, 16), -1),
+            array(new Point(15, 14), new Point(15, 15), -1),
+            array(new Point(15, 16), new Point(15, 15), 1),
+
+            array(new Point(15, 15), new Point(14, 15), 1),
+            array(new Point(15, 15), new Point(14, 14), 1),
+            array(new Point(15, 15), new Point(14, 16), 1),
+            array(new Point(15, 14), new Point(14, 15), 1),
+            array(new Point(15, 16), new Point(14, 15), 1),
+
+            array(new Point(15, 15), new Point(16, 15), -1),
+            array(new Point(15, 15), new Point(16, 14), -1),
+            array(new Point(15, 15), new Point(16, 16), -1),
+            array(new Point(15, 14), new Point(16, 15), -1),
+            array(new Point(15, 16), new Point(16, 15), -1),
+
+            array(new Point(14, 15), new Point(15, 15), -1),
+            array(new Point(14, 15), new Point(15, 14), -1),
+            array(new Point(14, 15), new Point(15, 16), -1),
+            array(new Point(14, 14), new Point(15, 15), -1),
+            array(new Point(14, 16), new Point(15, 15), -1),
+
+            array(new Point(16, 15), new Point(15, 15), 1),
+            array(new Point(16, 15), new Point(15, 14), 1),
+            array(new Point(16, 15), new Point(15, 16), 1),
+            array(new Point(16, 14), new Point(15, 15), 1),
+            array(new Point(16, 16), new Point(15, 15), 1),
+        );
+    }
 }
