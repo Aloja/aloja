0be9d741a2b770656735cced4d4d52e085137872
MortI2C <aaron.call@bsc.es>
2014-11-25 15:01:26 +0100

    Improved retrieving and bench scripts

--- shell/hdinsight/runhdibench.ps1
+++ shell/hdinsight/runhdibench.ps1
@@ -1,10 +1,13 @@
-param($clusterName,[String]$inputData="/example/data/1TB-sort-input",[String]$outputData="/example/data/1TB-sort-output", [String]$storageAccount, [String]$storageKey, [String]$containerName)
+param($clusterName,[String]$inputData="/example/data/1TB-sort-input",[String]$outputData="/example/data/1TB-sort-output", [String]$storageAccount, [String]$storageKey, [String]$containerName, [bool]$runTeragen=$true)
 
 . ./common.ps1
 
-DeleteStorageFile $inputData $storageAccount $storageKey $containerName
+if($runTeragen) {
+   DeleteStorageFile $inputData $storageAccount $storageKey $containerName
+}
+
 DeleteStorageFile $outputData $storageAccount $storageKey $containerName
-./runterasort.ps1
+./runterasort.ps1 -runTeragen $runteragen
 DeleteStorageFile $inputData $storageAccount $storageKey $containerName
 DeleteStorageFile $outputData $storageAccount $storageKey $containerName
 ./runsort.ps1
--- shell/hdinsight/runterasort.ps1
+++ shell/hdinsight/runterasort.ps1
@@ -1,9 +1,14 @@
-$teragen = New-AzureHDInsightMapReduceJobDefinition -JarFile "/example/jars/hadoop-mapreduce-examples.jar" -ClassName "teragen" -Arguments "1000000000", $inputData
-echo "Executing teragen"
-RunBench $teragen
-echo "Done teragen"
+param([bool]$runTeragen=$true)
+
+if($runTeragen) {
+	$teragen = New-AzureHDInsightMapReduceJobDefinition -JarFile "/example/jars/hadoop-mapreduce-examples.jar" -ClassName "teragen" -Arguments "1000000000", $inputData
+	echo "Executing teragen"
+	RunBench $teragen
+	echo "Done teragen"
+}
 
 echo "Executing terasort"
 $terasort = New-AzureHDInsightMapReduceJobDefinition -JarFile "/example/jars/hadoop-mapreduce-examples.jar" -ClassName "terasort" -Arguments $inputData, $outputData
 RunBench $terasort
 echo "Done terasort"
+
