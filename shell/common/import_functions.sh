65ecc251a21e353a6099ef48d2361849c9d98da8
npoggi <npmnpm@gmail.com>
2014-12-15 20:08:22 +0100

    Refactoring get_job_confs() to get Hadoop XML config (cherry picked from commit a5737c9)

--- shell/common/import_functions.sh
+++ shell/common/import_functions.sh
@@ -110,11 +110,9 @@ insert_conf_params_DB(){
 }
 
 get_job_confs() {
-
   id_exec_conf_params=""
   get_id_exec_conf_params "$exec"
 
-  logger "Attempting to get XML configuration for ID get_id_exec_conf_params"
   if [[ ! -z "$id_exec_conf_params" ]] ; then
     jobconfs=""
     #get Haddop conf files which are NOT jobs of prep
@@ -146,7 +144,7 @@ get_job_confs() {
     done
 
   else
-    logger "ERROR: cannot get id_exec for $bench_folder"
+    logger "ERROR: $bench_folder does not exist"
   fi
 }
 
