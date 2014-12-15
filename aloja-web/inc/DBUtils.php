fff2411bae40c93209ac6a0cb2a77fab599054fe
npoggi <npmnpm@gmail.com>
2014-12-15 23:29:20 +0100

    Add filters to execs

--- aloja-web/inc/DBUtils.php
+++ aloja-web/inc/DBUtils.php
@@ -92,7 +92,7 @@ class DBUtils
     {
         if($filter_execs === null)
             $filter_execs = "
-AND exe_time > 200 AND (id_cluster = 1 OR (bench != 'bayes' AND id_cluster=2))
+AND exe_time > 200
 #AND id_exec IN (select distinct (id_exec) from JOB_status where id_exec is not null)
 #AND id_exec IN (select distinct (id_exec) from SAR_cpu where id_exec is not null)
 ";
