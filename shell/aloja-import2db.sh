18bedb94613af1275ae080522772c529b08b72cc
npoggi <npmnpm@gmail.com>
2014-12-16 20:29:14 +0100

    Added moving to DONE folder succesful imports

--- shell/aloja-import2db.sh
+++ shell/aloja-import2db.sh
@@ -59,6 +59,7 @@ logger "Starting"
 
 for folder in 201* ; do
 
+  folder_OK="0"
   cd "$BASE_DIR" #make sure we come back to the starting folder
   logger "Iterating folder\t$folder CP: $(pwd)"
 
@@ -73,7 +74,7 @@ for folder in 201* ; do
     get_exec_params "log_${folder}.log" "$folder"
 
     if [[ -z $exec_params ]] ; then
-      logger "ERROR: cannot find exec details in log. Exiting folder..."
+      logger "ERROR: cannot find exec details in log. Exiting folder...\nTEST: $(grep  -e 'href' "log_${folder}.log" |grep 8099)"
       cd ..
       continue
     else
@@ -124,6 +125,8 @@ for folder in 201* ; do
 
         if [[ ! -z $exec_values ]] ; then
 
+          folder_OK="$(( folder_OK + 1 ))"
+
           insert="INSERT INTO execs (id_exec,id_cluster,exec,bench,exe_time,start_time,end_time,net,disk,bench_type,maps,iosf,replication,iofilebuf,comp,blk_size,zabbix_link)
                   VALUES (NULL, $cluster, \"$exec\", $exec_values)
                   ON DUPLICATE KEY UPDATE
@@ -163,148 +166,38 @@ for folder in 201* ; do
           #if dir does not exists or need to insert in DB
           if [[ "$REDO_ALL" == "1" || "$INSERT_DB" == "1" ]]  ; then
 			
-              #get the Hadoop job logs
-              job_files=$(find "./history/done" -type f -name "job*"|grep -v ".xml")
-
-              logger "Generating Hadoop Job CSVs for $bench_folder"
-              #rm -rf "hadoop_job"
-              #rm -rf "sysstat"
-              mkdir -p "hadoop_job"
-
-              for job_file in $job_files ; do
-                #Get hadoop job name
-                job_name="${job_file##*/}"
-                job_name="${job_name:0:21}"
-
-                logger "Processing Job $job_name File $job_file"
-
-                logger "Extrating Job history details for $job_file"
-                python2.7 "$CUR_DIR/job_history.py" -j "hadoop_job/${job_name}.details.csv" -d "hadoop_job/${job_name}.status.csv" -t "hadoop_job/${job_name}.tasks.csv" -i "$job_file"
-
-              done
-            fi
-
-            #DB inserting scripts
-            if [ "$INSERT_DB" == "1" ] ; then
-              #start with Hadoop's
-              cd hadoop_job
-              for csv_file in *.csv ; do
-                if [[ $(head $csv_file |wc -l) > 1 ]] ; then
-                  #get the job name and counter type from csv file name
-                  separator_pos=$(echo "$csv_file" | awk '{ print index($1,".")}')
-                  job_name="${csv_file:0:$(($separator_pos - 1))}"
-                  counter="${csv_file:$separator_pos:-4}"
-                  table_name="JOB_${counter}"
-                  logger "Inserting into DB $csv_file TN $table_name"
-                  #add host and missing data to csv
-                  awk "NR == 1 {\$1=\"id,id_exec,job_name,\"\$1; print } NR > 1 {\$1=\"NULL,${id_exec},${job_name},\"\$1; print }" "$csv_file" > tmp.csv
-
-                  insert_DB "${table_name}" "tmp.csv" "" ","
-                else
-                  logger "File $csv_file is INVALID\n$(cat $csv_file)"
-                fi
-              done
-              cd ..; logger "\n"
-
-              for sar_file in sar*.sar ; do
-                if [[ $(head $sar_file |wc -l) > 1 ]] ; then
-
-                  for table_name in "SAR_cpu" "SAR_io_paging" "SAR_interrupts" "SAR_load" "SAR_memory_util" "SAR_memory" "SAR_swap" "SAR_swap_util" "SAR_switches" "SAR_block_devices" "SAR_net_devices" "SAR_io_rate" "SAR_net_errors" "SAR_net_sockets"; do
-                    sar_command=""
-                    if [ "$table_name" == "SAR_cpu" ] ; then
-                      sar_command="-u"
-                    elif [ "$table_name" == "SAR_io_paging" ] ; then
-                      sar_command="-B"
-                    elif [ "$table_name" == "SAR_interrupts" ] ; then
-                      sar_command="-I SUM"
-                    elif [ "$table_name" == "SAR_load" ] ; then
-                      sar_command="-q"
-                    elif [ "$table_name" == "SAR_memory_util" ] ; then
-                      sar_command="-r"
-                    elif [ "$table_name" == "SAR_memory" ] ; then
-                      sar_command="-R"
-                    elif [ "$table_name" == "SAR_swap" ] ; then
-                      sar_command="-S"
-                    elif [ "$table_name" == "SAR_swap_util" ] ; then
-                      sar_command="-W"
-                    elif [ "$table_name" == "SAR_switches" ] ; then
-                      sar_command="-w"
-                    elif [ "$table_name" == "SAR_block_devices" ] ; then
-                      sar_command="-d"
-                    elif [ "$table_name" == "SAR_net_devices" ] ; then
-                      sar_command="-n ALL"
-                    elif [ "$table_name" == "SAR_io_rate" ] ; then
-                      sar_command="-b"
-                    elif [ "$table_name" == "SAR_net_errors" ] ; then
-                      sar_command="-n EDEV"
-                    elif [ "$table_name" == "SAR_net_sockets" ] ; then
-                      sar_command="-n SOCK"
-                    fi
-
-                    if [ "$sar_command" != "" ] ; then
-                      csv_name="$sar_file.$table_name.csv"
-                      $sadf -d "$sar_file" -- $sar_command |\
-                      sed 's/ UTC//g' | \
-                      awk "NR == 1 {sub(\"timestamp\", \"date\", \$2); sub(\"hostname\", \"host\", \$2); \
-                                    \$2=\"id;id_exec;\" \$2; print \$2} \
-                           NR > 1  {\$1=\"NULL;${id_exec};\"\$1;  print }" > "$csv_name"
-
-                      insert_DB "${table_name}" "$csv_name" "" ";"
-                    else
-                      logger "ERROR: no command for $table_name"
-                    fi
-                  done
-                else
-                  logger "ERROR: File $sar_file is INVALID"
-                fi
-              done
-
-              for vmstats_file in [vmstat-]*.log ; do
-                if [[ $(head $vmstats_file |wc -l) -gt 1 ]] ; then
-                  #get host name from file name
-                  hostn="${vmstats_file:7:-4}"
-                  table_name="VMSTATS"
-                  logger "Inserting into DB $vmstats_file TN $table_name"
-
-                  tail -n +2 "$vmstats_file" | awk '{out="";for(i=1;i<=NF;i++){out=out "," $i}}{print substr(out,2)}' | awk "NR == 1 {\$1=\"id_field,id_exec,host,time,\"\$1; print } NR > 1 {\$1=\"NULL,${id_exec},${hostn},\" (NR-2) \",\"\$1; print }" > tmp.csv
-
-                  insert_DB "${table_name}" "tmp.csv" "" ","
-                else
-                  logger "ERROR: File $vmstats_file is INVALID"
-                fi
-              done
-
-              for bwm_file in [bwm-]*.log ; do
-                if [[ $(head $bwm_file |wc -l) > 1 ]] ; then
-                  #get host name from file name
-                  hostn="${bwm_file:4:-4}"
-                  #there are two formats, 9 and 15 fields
-                  bwm_format="$(head -n 1 "$bwm_file" |grep -o ';'|wc -l)"
-                  logger "BWM format $bwm_format"
-                  head -n3 "$bwm_file"
-                  if [[ $bwm_format -gt 9 ]] ; then
-                    table_name="BWM2"
-                    cat "$bwm_file" | awk "NR == 1 {print \"id;id_exec;host;timestamp;iface_name;bytes_out_s;bytes_in_s;bytes_total_s;bytes_in;bytes_out;packets_out_s;packets_in_s;packets_total_s;packets_in;packets_out;errors_out_s;errors_in_s;errors_in;errors_out\"} NR > 1 {\$1=\"NULL;${id_exec};${hostn};\"\$1; print }" > tmp.csv
-                  else
-                    table_name="BWM"
-                    cat "$bwm_file" | awk "NR == 1 {print \"id;id_exec;host;unix_timestamp;iface_name;bytes_out;bytes_in;bytes_total;packets_out;packets_in;packets_total;errors_out;errors_in\"} NR > 1 {\$1=\"NULL;${id_exec};${hostn};\"\$1; print }" > tmp.csv
-                  fi
-
-                  logger "Inserting into DB $bwm_file TN $table_name"
-
-                  insert_DB "${table_name}" "tmp.csv" "" ";"
-                else
-                  logger "File $bwm_file is INVALID"
-                fi
-              done
-            fi
+              extract_hadoop_jobs
           fi
-          cd ..; logger "Leaving folder $bench_folder\n"
+
+          #DB inserting scripts
+          if [ "$INSERT_DB" == "1" ] ; then
+            #start with Hadoop's
+            import_hadoop_jobs
+
+            import_sar_files
+
+            import_vmstats_files
+
+            import_bwm_files
+
+          fi
+        fi
+        cd ..; logger "Leaving folder $bench_folder\n"
+
       else
         logger "ERROR: cannot find folder $bench_folder\nLS: $(ls -lah)"
       fi
     done #end for bzip file
     cd ..; logger "Leaving folder $folder\n"
+
+    if (( "$folder_OK" >= 3 )) ; then
+      logger "OK=$folder_OK Moving folder $folder to DONE"
+      mkdir -p "$BASE_DIR/DONE"
+      mv "$BASE_DIR/$folder" "$BASE_DIR/DONE/"
+    else
+      logger "OK=$folder_OK Leaving folder $folder out to revise"
+    fi
+
   else
     [ ! -d "$folder" ] && logger "ERROR: $folder not a folder, continuing."
     [ "$folder_time" -gt "$min_time" ] && logger "ERROR: Folder time: $folder_time not greater than Min time: $min_time"
