a1d5b3e41dc8f2716a99ecb988a5fb14f9c8ee2f
npoggi <npmnpm@gmail.com>
2015-01-21 16:27:41 +0100

    Adding minerva100 as a provider

---
+++ aloja-deploy/providers/minerva100.sh
@@ -0,0 +1,54 @@
+CUR_DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CUR_DIR_TMP/on-premise.sh"
+
+#overrides and custom minerva100 functions
+
+
+#minerva needs *real* user first
+get_ssh_user() {
+
+  #check if we can change from root user
+  if [ "$requireRootFirst" ] ; then
+    #"WARNINIG: connecting as root"
+    echo "npoggi"
+  else
+    echo "$userAloja"
+  fi
+}
+
+vm_initial_bootstrap() {
+
+  local bootstrap_file="Initial_Bootstrap"
+
+  if check_bootstraped "$bootstrap_file" ""; then
+    logger "Bootstraping $vm_name "
+
+    local home_prefix="/users/scratch"
+    vm_execute "
+sudo useradd --create-home --home $home_prefix/$userAloja -s /bin/bash $userAloja;
+sudo echo -n '$userAloja:$passwordAloja' |sudo chpasswd;
+sudo adduser $userAloja sudo;
+sudo sed -i.bkp -e 's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers;
+
+sudo mkdir -p $home_prefix/$userAloja/.ssh;
+sudo bash -c \"echo '${insecureKey}' >> sudo $home_prefix/$userAloja/.ssh/authorized_keys\";
+sudo chown -R $userAloja: $home_prefix/$userAloja/.ssh;
+sudo cp $home_prefix/$userAloja/.profile $home_prefix/$userAloja/.bashrc /root/;
+
+sudo adduser $userAloja adm;
+"
+
+    test_action="$(vm_execute " [ -f $home_prefix/$userAloja/.ssh/authorized_keys ] && echo '$testKey'")"
+
+    if [ "$test_action" == "$testKey" ] ; then
+      #set the lock
+      check_bootstraped "$bootstrap_file" "set"
+    else
+      logger "ERROR at $bootstrap_file for $vm_name. Test output: $test_action"
+    fi
+
+  else
+    logger "$bootstrap_file already configured"
+  fi
+}
