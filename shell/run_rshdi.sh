c67080133ab77ab0a72c126569e0302b239d20ec
MortI2C <aaron.call@bsc.es>
2015-02-02 14:43:06 +0100

    initial version RS Big Data bench execs

--- shell/run_rshdi.sh
+++ shell/run_rshdi.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-CUR_DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONF_DIR="$CUR_DIR_TMP/common/"
-source "$CUR_DIR_TMP/common/include_benchmarks.sh"
-
 if [ "$#" -ne 1 ]; then
 	echo "Usage: run_rshdi.sh clustername"
 	exit	
@@ -12,26 +8,27 @@ fi
 clusterName=$1
 
 HDD="/home/pristine"
-
 DSH="dsh -aM -c"
 
 
 if [ ! -d /logs ]; then
 	sudo mkdir /logs
+	sudo chown -R pristine.pristine /logs
+	chmod 777 /logs -R
 fi
 
 installIfNotInstalled() {
  if $DSH "yum list installed \"$@\"" > /dev/null 2>&1; then
-     echo "Package $@ already installed"
+    logger "Package $@ already installed"
   else
-    $DSH "sudo yum install \"$@\""
-    echo "Package $@ installed"
+    $DSH "yum install -y -q \"$@\""
+    logger "Package $@ installed"
   fi
 }
 
 installDsh() {
   if [ ! -e /usr/local/bin/dsh ]; then
-    echo "Installing DSH"
+    logger "Installing DSH"
     wget http://www.netfort.gr.jp/~dancer/software/downloads/libdshconfig-0.20.10.cvs.1.tar.gz
     tar xfz libdshconfig*.tar.gz 
 	cd libdshconfig-*
@@ -49,18 +46,21 @@ installDsh() {
 	sudo echo 'remoteshellopt=-oStrictHostKeyChecking=no' >> /usr/local/etc/dsh.conf
 		
 	echo -e "Host *\nIdentityFile = /home/pristine/id_rsa" > .ssh/config
+	
+	logger "DSH successfully installed"
  fi
 }
 
 restart_monit(){
+ 
   logger "Restart Monit"
 
   stop_monit
  $DSH "rm $HDD/vmstat.log $HDD/bwm.log $HDD/sar-output.sar"
 
-  $DSH "vmstat -n 1 >> $HDD/vmstat-`hostname`.log &" 2>&1 |tee -a $LOG_PATH
-  $DSH "bwm-ng -o csv -I eth1 -u bytes -t 1000 >> $HDD/bwm-`hostname`.log &" 2>&1 |tee -a $LOG_PATH
-  $DSH "sar -o $HDD/sar-output-`hostname`.sar 1 > /dev/null 2>&1 &" 2>&1 |tee -a $LOG_PATH
+  $DSH "vmstat -n 1 >> $HDD/vmstat-`hostname`.log &" 2>&1
+  $DSH "bwm-ng -o csv -I eth1 -u bytes -t 1000 >> $HDD/bwm-`hostname`.log &" 2>&1
+  $DSH "sar -o $HDD/sar-output-`hostname`.sar 1 > /dev/null 2>&1 &" 2>&1
 
   logger "Monit ready"
 }
@@ -76,11 +76,12 @@ stop_monit(){
 }
 
 collect_logs(){
-	mkdir "/logs/$1"
+	logger "Retrieving logs"
+	mkdir -m 777 "/logs/$1"
 	sudo su hdfs -c "hdfs dfs -copyToLocal /mr-history /logs/$1"
-	$DSH "scp -r $HDD/vmstat-* localhost:/logs/$1" 2>&1 | tee -a $LOG_PATH
-	$DSH "scp -r $HDD/bwm-* localhost:/logs/$1" 2>&1 | tee -a $LOG_PATH
-	$DSH "scp -r $HDD/sar-output-* localhost:/logs/$1" 2>&1 | tee -a $LOG_PATH	
+	$DSH "scp -r $HDD/vmstat-* localhost:/logs/$1" 2>&1
+	$DSH "scp -r $HDD/bwm-* localhost:/logs/$1" 2>&1
+	$DSH "scp -r $HDD/sar-output-* localhost:/logs/$1" 2>&1
 }
 
 installIfNotInstalled "sysstat"
@@ -88,10 +89,13 @@ installIfNotInstalled "bwm-ng"
 installDsh
 
 restart_monit
-hadoop dfs -rmr 100GB-terasort-input
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen 1000000000 100GB-terasort-input
-hadoop dfs -rmr 100GB-terasort-output
+logger "Starting run of teragen"
+hdfs dfs -rm -r 100GB-terasort-input
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen 1000 100GB-terasort-input
+logger "Starting run of terasort"
+hdfs dfs -rm -r 100GB-terasort-output
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort 100GB-terasort-input 100GB-terasort-output
+logger "Terasort ended"
 stop_monit
 
 collect_logs "$clusterName-terasort-`date +%s`"
