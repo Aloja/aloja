dab13c7899edfadacc06bf127006d906561c92af
npoggi <npmnpm@gmail.com>
2015-02-04 15:52:31 +0100

    Changed minerva-1000 cluster name to include ID

--- shell/common/create_db.sh
+++ shell/common/create_db.sh
@@ -53,7 +53,7 @@ link varchar(255),
 datanodes int DEFAULT NULL,
 primary key (id_cluster)) engine InnoDB;
 
-insert ignore into clusters set name='m-1000',     id_cluster=1, cost_hour=12, type='Colocated', link='http://hadoop.bsc.es/?page_id=51';
+insert ignore into clusters set name='m1000-01',     id_cluster=1, cost_hour=12, type='Colocated', link='http://hadoop.bsc.es/?page_id=51';
 insert ignore into clusters set name='al-02', id_cluster=2, cost_hour=7, type='IaaS Cloud', link='http://www.windowsazure.com/en-us/pricing/calculator/';
 
 #TODO move this to end of execution
