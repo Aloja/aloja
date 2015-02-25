c1ea7fcb8b6d674b1b88377fdaefbfc6108acf96
Josep Lluís Berral García <josepllberral@gmail.com>
2015-02-25 15:18:23 +0100

    Added new attributes to ml_outliers. Expanded aloja_cli.r. Created functions for find instance and find model.

--- aloja-web/Controller/MLOutliersController.php
+++ aloja-web/Controller/MLOutliersController.php
@@ -8,6 +8,92 @@ use alojaweb\inc\DBUtils;
 
 class MLOutliersController extends AbstractController
 {
+	/* GENERAL FUNCTIONS TO USE */
+
+	private function generateModelInfo($param_names, $params, $condition)
+	{
+	    	$db = $this->container->getDBUtils();
+		$filter_options = Utils::getFilterOptions($db);
+		$paramAllOptions = $tokens = array();
+		$model_info = '';
+		foreach ($param_names as $p) 
+		{
+			if (array_key_exists(substr($p,0,-1),$filter_options)) $paramAllOptions[$p] = array_column($filter_options[substr($p,0,-1)],substr($p,0,-1));
+			if ($condition) $model_info = $model_info.((empty($params[$p]))?' '.substr($p,0,-1).' ("*")':' '.substr($p,0,-1).' ("'.implode('","',$params[$p]).'")');	
+			else $model_info = $model_info.((empty($params[$p]))?' '.substr($p,0,-1).' ("'.implode('","',$paramAllOptions[$p]).'")':' '.substr($p,0,-1).' ("'.implode('","',$params[$p]).'")');
+		}
+		return $model_info;
+	}
+
+	private function generateSimpleInstance($param_names, $params, $condition)
+	{
+	    	$db = $this->container->getDBUtils();
+		$filter_options = Utils::getFilterOptions($db);
+		$paramAllOptions = $tokens = array();
+		$instance = '';
+		foreach ($param_names as $p) 
+		{
+			if (array_key_exists(substr($p,0,-1),$filter_options)) $paramAllOptions[$p] = array_column($filter_options[substr($p,0,-1)],substr($p,0,-1));
+
+			$tokens[$p] = '';
+			if ($condition && empty($params[$p])) { $tokens[$p] = '*'; }
+			elseif (!$condition && empty($params[$p]))  { foreach ($paramAllOptions[$p] as $par) $tokens[$p] = $tokens[$p].(($tokens[$p] != '')?'|':'').(($p=='comps')?'Cmp':'').(($p=='id_clusters')?'Cl':'').$par; }
+			else { foreach ($params[$p] as $par) $tokens[$p] = $tokens[$p].(($tokens[$p] != '')?'|':'').(($p=='comps')?'Cmp':'').(($p=='id_clusters')?'Cl':'').$par; }
+			$instance = $instance.(($instance=='')?'':',').$tokens[$p];
+		}
+		return $instance;
+	}
+
+	private function findMatchingModels ($model_info, &$possible_models, &$possible_models_id)
+	{
+		
+		if (($fh = fopen(getcwd().'/cache/query/record.data', 'r')) !== FALSE)
+		{
+			while (!feof($fh))
+			{
+				$line = fgets($fh, 4096);
+				if (preg_match("(((bench|net|disk|blk_size) (\(.+\)))( )?)", $line))
+				{
+					$fts = explode(" : ",$line);
+					$parts = explode(" ",$fts[1]);
+					$buffer = array();
+					$last_part = "";
+					foreach ($parts as $p)
+					{
+						if (preg_match("(\(.+\))", $p)) $buffer[$last_part] = explode(",",str_replace(array('(',')','"'),'',$p));
+						else $last_part = $p;
+					}
+
+					if ($model_info[0]==' ') $model_info = substr($model_info, 1);
+					$parts_2 = explode(" ",$model_info);
+					$buffer_2 = array();
+					$last_part = "";
+					foreach ($parts_2 as $p)
+					{
+						if (preg_match("(\(.+\))", $p)) $buffer_2[$last_part] = explode(",",str_replace(array('(',')','"'),'',$p));
+						else $last_part = $p;
+					}
+
+					$match = TRUE;
+					foreach ($buffer_2 as $bk => $ba)
+					{
+						if (!array_key_exists($bk,$buffer)) { $match = FALSE; break; }
+						if ($buffer[$bk][0] != "*" && array_intersect($ba, $buffer[$bk]) != $ba) { $match = FALSE; break; }
+					}
+
+					if ($match)
+					{
+						$possible_models[] = $line;
+						$possible_models_id[] = $fts[0];
+					}
+				}
+			}
+			fclose($fh);
+		}
+	}
+
+	/* CONTROLLER FUNCTIONS */
+
 	public function mloutliersAction()
 	{
 		$jsonData = $jsonWarns = $jsonOuts = array();
@@ -39,64 +125,13 @@ class MLOutliersController extends AbstractController
 				$params['replications'] = array('1'); $where_configs .= ' AND replication IN ("1")';
 			}
 
-			$filter_options = Utils::getFilterOptions($db);
-			$paramAllOptions = $tokens = array();
-			$model_info = '';
-			foreach ($param_names as $p) 
-			{
-				if (array_key_exists(substr($p,0,-1),$filter_options)) $paramAllOptions[$p] = array_column($filter_options[substr($p,0,-1)],substr($p,0,-1));
-				$model_info = $model_info.((empty($params[$p]))?' '.substr($p,0,-1).' ("'.implode('","',$paramAllOptions[$p]).'")':' '.substr($p,0,-1).' ("'.implode('","',$params[$p]).'")');	
-			
- 				$tokens[$p] = '';
-				if (empty($params[$p])) { foreach ($paramAllOptions[$p] as $par) $tokens[$p] = $tokens[$p].(($tokens[$p] != '')?'|':'').(($p=='comps')?'Cmp':'').(($p=='id_clusters')?'Cl':'').$par; }
- 				else { foreach ($params[$p] as $par) $tokens[$p] = $tokens[$p].(($tokens[$p] != '')?'|':'').(($p=='comps')?'Cmp':'').(($p=='id_clusters')?'Cl':'').$par; }
- 				$instance = $instance.(($instance=='')?'':',').$tokens[$p];
- 			}
+			// compose instance
+			$instance = $this->generateSimpleInstance($param_names, $params, true);
+			$model_info = $this->generateModelInfo($param_names, $params, true);
 
-			// Model for filling
-			if (($fh = fopen(getcwd().'/cache/query/record.data', 'r')) !== FALSE)
-			{
-				while (!feof($fh))
-				{
-					$line = fgets($fh, 4096);
-					if (preg_match("(((bench|net|disk|blk_size) (\(.+\)))( )?)", $line))
-					{
-						$fts = explode(" : ",$line);
-						$parts = explode(" ",$fts[1]);
-						$buffer = array();
-						$last_part = "";
-						foreach ($parts as $p)
-						{
-							if (preg_match("(\(.+\))", $p)) $buffer[$last_part] = explode(",",str_replace(array('(',')','"'),'',$p));
-							else $last_part = $p;
-						}
-
-						if ($model_info[0]==' ') $model_info = substr($model_info, 1);
-						$parts_2 = explode(" ",$model_info);
-						$buffer_2 = array();
-						$last_part = "";
-						foreach ($parts_2 as $p)
-						{
-							if (preg_match("(\(.+\))", $p)) $buffer_2[$last_part] = explode(",",str_replace(array('(',')','"'),'',$p));
-							else $last_part = $p;
-						}
-
-						$match = TRUE;
-						foreach ($buffer_2 as $bk => $ba)
-						{
-							if (!array_key_exists($bk,$buffer)) { $match = FALSE; break; }
-							if ($buffer[$bk][0] != "*" && array_intersect($ba, $buffer[$bk]) != $ba) { $match = FALSE; break; }
-						}
-
-						if ($match)
-						{
-							$possible_models[] = $line;
-							$possible_models_id[] = $fts[0];
-						}
-					}
-				}
-				fclose($fh);
-			}
+			// model for filling
+			$possible_models = $possible_models_id = array();
+			$this->findMatchingModels($model_info, $possible_models, $possible_models_id);
 
 			$model = '';
 			if (!empty($possible_models_id))
@@ -105,9 +140,11 @@ class MLOutliersController extends AbstractController
 				else $model = $possible_models_id[0];
 
 				$cache_ds = getcwd().'/cache/query/'.md5($model_info.'-'.$model).'-cache.csv';
-				$in_process = shell_exec('ps aux | grep "aloja_outlier_dataset -d '.$cache_ds.' -l '.$model.'" | grep -v grep');
 
-				if (file_exists($cache_ds) && $in_process == NULL)
+				$is_cached = file_exists($cache_ds);
+				$in_process = file_exists(getcwd().'/cache/query/'.md5($model_info.'-'.$model).'.lock');
+
+				if ($is_cached && !$in_process)
 				{
 					$keep_cache = TRUE;
 					foreach (array("cause.csv", "resolutions.csv", "object.rds") as &$value)
@@ -121,34 +158,20 @@ class MLOutliersController extends AbstractController
 					}
 				}
 
-				if (!file_exists($cache_ds) && $in_process == NULL)
+				if (!$is_cached && !$in_process)
 				{
 					// get headers for csv
 					$header_names = array(
-						'id_exec' => 'ID','bench' => 'Benchmark','exe_time' => 'Exe Time','exec' => 'Exec Conf','cost' => 'Running Cost $','net' => 'Net',
-						'disk' => 'Disk','maps' => 'Maps','iosf' => 'IO SFac','replication' => 'Rep','iofilebuf' => 'IO FBuf','comp' => 'Comp',
-						'blk_size' => 'Blk size','id_cluster' => 'Cluster','histogram' => 'Histogram','prv' => 'PARAVER','end_time' => 'End time',
+						'id_exec' => 'ID','bench' => 'Benchmark','exe_time' => 'Exe.Time','net' => 'Net','disk' => 'Disk','maps' => 'Maps','iosf' => 'IO.SFac',
+						'replication' => 'Rep','iofilebuf' => 'IO.FBuf','comp' => 'Comp','blk_size' => 'Blk.size','e.id_cluster' => 'Cluster','name' => 'Cl.Name',
+						'datanodes' => 'Datanodes','headnodes' => 'Headnodes','vm_OS' => 'VM.OS','vm_cores' => 'VM.Cores','vm_RAM' => 'VM.RAM',
+						'provider' => 'Provider','vm_size' => 'VM.Size','type' => 'Type'
 					);
-
-				    	$query="SHOW COLUMNS FROM execs;";
-				    	$rows = $db->get_rows ($query);
-					if (empty($rows)) throw new \Exception('No data matches with your critteria.');
-					$headers = array();
-					$names = array();
-					$count = 0;
-					foreach($rows as $row)
-					{
-						if (array_key_exists($row['Field'],$header_names))
-						{
-							$headers[$count] = $row['Field'];
-							$names[$count++] = $header_names[$row['Field']];
-						}
-					}
-					$headers[$count] = 0;	// FIXME - Costs are NOT in the database?! What sort of anarchy is this?!
-					$names[$count++] = $header_names['cost'];
+					$headers = array_keys($header_names);
+					$names = array_values($header_names);
 
 					// dump the result to csv
-				    	$query="SELECT ".implode(",",$headers)." FROM execs WHERE valid = TRUE ".$where_configs.";";
+				    	$query="SELECT ".implode(",",$headers)." FROM execs e LEFT JOIN clusters c ON e.id_cluster = c.id_cluster WHERE e.valid = TRUE AND e.exe_time > 100".$where_configs.";";
 				    	$rows = $db->get_rows ( $query );
 
 					if (empty($rows)) throw new \Exception('No data matches with your critteria.');
@@ -163,7 +186,10 @@ class MLOutliersController extends AbstractController
 					}
 
 					// launch query
-					$command = 'cd '.getcwd().'/cache/query; '.getcwd().'/resources/aloja_cli.r -m aloja_outlier_dataset -d '.$cache_ds.' -l '.$model.' -p sigma=3:hdistance=3:saveall='.md5($model_info.'-'.$model).' > /dev/null &';
+					$command = '( cd '.getcwd().'/cache/query ; ';
+					$command = $command.'touch '.getcwd().'/cache/query/'.md5($model_info.'-'.$model).'.lock ; ';
+					$command = $command.getcwd().'/resources/aloja_cli.r -m aloja_outlier_dataset -d '.$cache_ds.' -l '.$model.' -p sigma=3:hdistance=3:saveall='.md5($model_info.'-'.$model).' > /dev/null 2>&1 ; ';
+					$command = $command.'rm -f '.getcwd().'/cache/query/'.md5($model_info.'-'.$model).'.lock ; ) > /dev/null 2>&1 &';
 					exec($command);
 
 					// update cache record (for human reading)
@@ -171,16 +197,16 @@ class MLOutliersController extends AbstractController
 					shell_exec("sed -i '/".$register."/d' ".getcwd()."/cache/query/record.data");
 					file_put_contents(getcwd().'/cache/query/record.data', $register, FILE_APPEND | LOCK_EX);
 				}
-				$in_process = shell_exec('ps aux | grep "aloja_outlier_dataset -d '.$cache_ds.' -l '.$model.'" | grep -v grep');
-				$must_wait = 'NO';
+				$in_process = file_exists(getcwd().'/cache/query/'.md5($model_info.'-'.$model).'.lock');
 
-				if ($in_process != NULL)
+				if ($in_process)
 				{
 					$jsonData = $jsonOuts = $jsonWarns = $jsonHeader = $jsonTable = '[]';
 					$must_wait = 'YES';
 				}
 				else
 				{
+					$must_wait = 'NO';
 					if (isset($_GET['dump']))
 					{
 						try
