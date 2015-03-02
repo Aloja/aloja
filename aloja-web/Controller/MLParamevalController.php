81963b548d92fb1d0634f24038744aceb9c72bd5
Josep Lluís Berral García <josepllberral@gmail.com>
2015-03-02 11:32:28 +0100

    Added commands to queue processing

--- aloja-web/Controller/MLParamevalController.php
+++ aloja-web/Controller/MLParamevalController.php
@@ -123,7 +123,7 @@ class MLParamevalController extends AbstractController
 				if (!$is_cached && !$in_process && !$finished_process)
 				{
 					// drop query
-					$command = '( cd '.getcwd().'/cache/query ; ';
+					$command = getcwd().'/resources/queue -c "( cd '.getcwd().'/cache/query ; ';
 					$command = $command.'touch '.getcwd().'/cache/query/'.md5($instance.'-'.$model).'.lock ; ';
 					$command = $command.'rm -f '.$tmp_file.' ';
 					foreach ($instances as $inst)
@@ -131,7 +131,7 @@ class MLParamevalController extends AbstractController
 						$command = $command.'&& '.getcwd().'/resources/aloja_cli.r -m aloja_predict_instance -l '.$model.' -p inst_predict=\''.$inst.'\' -v | grep -v \'WARNING\' | grep -v \'Prediction\' >> '.$tmp_file.' ';
 					}
 					$command = $command.'&& touch  '.getcwd().'/cache/query/'.md5($instance.'-'.$model).'.ready; ';
-					$command = $command.'rm -f '.getcwd().'/cache/query/'.md5($instance.'-'.$model).'.lock ; ) > /dev/null 2>&1 &';
+					$command = $command.'rm -f '.getcwd().'/cache/query/'.md5($instance.'-'.$model).'.lock ; ) > /dev/null 2>&1 "&';
 					exec($command);
 				}
 
