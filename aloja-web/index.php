60a5031de3f7971267d7010574426a34067cca78
npoggi <npmnpm@gmail.com>
2015-03-10 13:54:14 +0100

    Changed to hide certain providers by default

--- aloja-web/index.php
+++ aloja-web/index.php
@@ -38,7 +38,7 @@ try {
     $controller->$controllerMethod['method']();
 } catch (\Exception $e) {
     if($container->get('config')['enable_debug'])
-      exit('Unexpected error: '.$e->getMessage(). "\n".$e->getPrevious());
+      exit('Unexpected error: '.$e->getMessage(). "\n".$e->getPrevious().print_r($e, 1));
     else {
       $container->getLog()->addError('Internal server error: '.$e->getMessage(). "\n".$e->getPrevious());
       $container->displayServerError();
