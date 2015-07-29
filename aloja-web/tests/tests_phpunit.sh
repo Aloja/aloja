0b400d7b02045194342004c39e86bcc2f244504d
MortI2C <aaron.call@bsc.es>
2015-07-29 18:33:35 +0200

    WEB: fixed phpunit tests script

--- aloja-web/tests/tests_phpunit.sh
+++ aloja-web/tests/tests_phpunit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-( cd ${BASE_DIR}/aloja-web/ ; ${BASE_DIR}/aloja-web/vendor/bin/phpunit -c ${BASE_DIR}/aloja-web/phpunit.xml )
+( cd ${BASE_DIR}/.. ; ${BASE_DIR}/../vendor/bin/phpunit -c ${BASE_DIR}/../phpunit.xml )
