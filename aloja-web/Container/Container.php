dbeb337380e43ec962aa59baa62718813f60ef1d
npoggi <npmnpm@gmail.com>
2015-09-14 19:29:42 +0200

    WEB: fixed in_dev() function

--- aloja-web/Container/Container.php
+++ aloja-web/Container/Container.php
@@ -23,7 +23,6 @@ class Container
 	 */
     public function in_dev()
     {
-return true;
         if (isset($_SERVER['HTTP_CLIENT_IP'])
                 || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
                 || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '10.0.2.2', '192.168.99.1'))) {
