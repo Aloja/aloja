c8dab226384893b6553465141bdfc4a0a821784a
npoggi <npmnpm@gmail.com>
2015-10-08 19:26:32 +0200

    PROD ONLY: changing DB table alias

--- aloja-web/inc/DBUtils.php
+++ aloja-web/inc/DBUtils.php
@@ -49,6 +49,7 @@ class DBUtils
 
     public function get_rows($sql, $params = array())
     {
+echo "$sql";
         $md5_sql = md5($sql.http_build_query($params, '', ','));
         $file_path = "{$this->container['config']['db_cache_path']}/CACHE_$md5_sql.sql";
 
