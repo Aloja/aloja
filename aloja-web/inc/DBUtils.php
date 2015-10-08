149f326900687bcedf0de64426b4f56bbdf5b11d
npoggi <npmnpm@gmail.com>
2015-10-08 19:28:02 +0200

    PROD ONLY: changing DB table alias

--- aloja-web/inc/DBUtils.php
+++ aloja-web/inc/DBUtils.php
@@ -49,7 +49,6 @@ class DBUtils
 
     public function get_rows($sql, $params = array())
     {
-echo "$sql";
         $md5_sql = md5($sql.http_build_query($params, '', ','));
         $file_path = "{$this->container['config']['db_cache_path']}/CACHE_$md5_sql.sql";
 
@@ -103,7 +102,7 @@ echo "$sql";
 
     public static function getFilterExecs($execsAlias = "e")
     {
-        return " AND $execsAlias.id_cluster NOT IN (select id_cluster from clusters where id_cluster = '35' OR  provider = 'rackspacecbd') " ;
+        return " AND execs.id_cluster NOT IN (select id_cluster from clusters where id_cluster = '35' OR  provider = 'rackspacecbd') " ;
 
         if (isset($_COOKIE['g']) && $_COOKIE['g'] == 'godmode') {
             return " " ;
