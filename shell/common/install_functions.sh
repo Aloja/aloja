6a8d9d3f57cef42a43091ba36fa666ad953d4307
waldner <waldner@katamail.com>
2015-11-03 13:38:48 +0100

    install bwm-ng by default

--- shell/common/install_functions.sh
+++ shell/common/install_functions.sh
@@ -97,7 +97,7 @@ vm_install_base_packages() {
   if check_bootstraped "$bootstrap_file" ""; then
     logger "Installing packages for for VM $vm_name "
 
-    install_packages "ssh dsh rsync sshfs sysstat gawk libxml2-utils ntp wget curl unzip wamerican" "update" #wamerican is for hivebench
+    install_packages "ssh dsh rsync sshfs sysstat gawk libxml2-utils ntp wget curl unzip wamerican bwm-ng" "update" #wamerican is for hivebench
 
     local test_action="$(vm_execute "sar -V |grep 'Sebastien Godard' && dsh --version |grep 'Junichi' && echo '$testKey'")" #checks for sysstat
     if [[ "$test_action" == *"$testKey"* ]] ; then
