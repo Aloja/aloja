5dc5473d72a2700870dabf54d9c42074a5ddc131
npoggi <npmnpm@gmail.com>
2016-04-11 12:19:15 +0200

    BENCH: force sync folders to backup server

--- shell/common/common_benchmarks.sh
+++ shell/common/common_benchmarks.sh
@@ -1107,12 +1107,12 @@ rsync_extenal() {
     local job_folder="$1"
     local job_folder_full_path="$(get_repo_path)jobs_$clusterName/$job_folder"
 
-    if [ ! -d "$job_folder_full_path" ] ; then
+#    if [ ! -d "$job_folder_full_path" ] ; then
       logger "INFO: Rsyncing results to external server"
       vm_rsync_from "$(get_repo_path)/jobs_$clusterName/$job_folder" "$remoteFileServer:~/share/jobs_$clusterName/" "$remoteFileServerPort" "--progress" "$remoteFileServerProxy"
-    else
-      logger "WARNING: path $job_folder_full_path is not a directory"
-    fi
+#    else
+#      logger "WARNING: path $job_folder_full_path is not a directory"
+#    fi
   else
     logger "DEBUG: No remote file server defined to send results"
   fi
