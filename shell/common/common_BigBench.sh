9a8335a7ef9a794aa6b631b1072c3016df826fc6
alejandromontero <alejandro.montero.rivero@gmail.com>
2016-08-01 13:50:10 +0200

    BigBench: added support for both hive and spark

--- shell/common/common_BigBench.sh
+++ shell/common/common_BigBench.sh
@@ -30,18 +30,11 @@ set_BigBench_requires() {
 get_BigBench_exports() {
   local to_export
 
-  if (( "$ENGINE" == "hive" )) ; then
-    to_export="
-    $(get_hive_exports)
-    PATH=$PATH:$BENCH_HADOOP_DIR/bin/:$(get_local_apps_path)/mahout-distribution-${MAHOUT_VERSION}/bin/
-    "
-  else
-    to_export="
-    $(get_hive_exports)
-    $(get_spark_exports)
-    PATH=$PATH:$BENCH_HADOOP_DIR/bin/:$(get_local_apps_path)/mahout-distribution-${MAHOUT_VERSION}/bin/
-    "
-  fi
+  to_export="
+  $(get_hive_exports)
+  $(get_spark_exports)
+  PATH=$PATH:$BENCH_HADOOP_DIR/bin/:$(get_local_apps_path)/mahout-distribution-${MAHOUT_VERSION}/bin/
+  "
 
   echo -e "$to_export\n"
 }
