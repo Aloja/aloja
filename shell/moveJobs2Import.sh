e26ec45326ec8102573c0c4f172b68f6b2aaa7ac
npoggi <npmnpm@gmail.com>
2016-10-19 15:08:47 +0200

    BENCH: updating years in import functions

--- shell/moveJobs2Import.sh
+++ shell/moveJobs2Import.sh
@@ -28,7 +28,7 @@ for jobs_folder in $SHARE_DIR/jobs_* ; do
 
   for exec_folder in 201* ; do
     if [ -d "$exec_folder" ] ; then
-      if [ "2015" == "${exec_folder:0:4}" ] ; then
+      if (( "${exec_folder:0:4}" > "2015" )) ; then
         #make sure there is a finished benchmark
         #if [ "$(ls *.tar.bz2 2> /dev/null |grep -v 'host_conf.tar.bz2'|grep -v 'prep_')" ] ; then
         if [ "$(ls $exec_folder/*.tar.bz2 2> /dev/null |egrep -v -e 'host_conf.tar.bz2' -e 'prep_.*.tar.bz2')" ] ; then
@@ -42,7 +42,7 @@ for jobs_folder in $SHARE_DIR/jobs_* ; do
           logger "ERROR: cannot find benchmark in $exec_folder"
         fi
       else
-        logger "ERROR: $exec_folder year not 2015!"
+        logger "ERROR: $exec_folder year not > 2015!"
       fi
     else
       logger "ERROR: $exec_folder not a folder!"
