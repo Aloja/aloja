f390befc495604a322def0e28403bcea4768e6e6
npoggi <npmnpm@gmail.com>
2016-11-21 11:19:15 +0100

    BENCH: refactor of spark benchmarks

--- shell/common/benchmark_D2F-Bench-spark.sh
+++ shell/common/benchmark_D2F-Bench-spark.sh
@@ -1,21 +1,34 @@
 # TPC-H benchmark from Todor Ivanov https://github.com/t-ivanov/D2F-Bench/
-# Spark version
-# Benchmark to test Hive installation and configurations
+# Benchmark to test Spark installation and configurations
 
 source_file "$ALOJA_REPO_PATH/shell/common/common_TPC-H.sh"
 
 source_file "$ALOJA_REPO_PATH/shell/common/common_spark.sh"
-#set_spark_requires
+set_spark_requires
+
 
 benchmark_suite_run() {
   logger "INFO: Running $BENCH_SUITE"
 
   tpc-h_datagen
 
-  for query in $BENCH_LIST ; do
-    logger "INFO: RUNNING $query"
-    execute_query_spark "$query"
+  BENCH_CURRENT_NUM_RUN="1" #reset the global counter
 
+  # Iterate at least one time
+  while true; do
+    [ "$BENCH_NUM_RUNS" ] && logger "INFO: Starting iteration $BENCH_CURRENT_NUM_RUN of $BENCH_NUM_RUNS"
+
+    for query in $BENCH_LIST ; do
+      logger "INFO: RUNNING $query of $BENCH_NUM_RUNS runs"
+      execute_query_spark "$query"
+    done
+
+    # Check if requested to iterate multiple times
+    if [ ! "$BENCH_NUM_RUNS" ] || [[ "$BENCH_CURRENT_NUM_RUN" -ge "$BENCH_NUM_RUNS" ]] ; then
+      break
+    else
+      BENCH_CURRENT_NUM_RUN="$((BENCH_CURRENT_NUM_RUN + 1))"
+    fi
   done
 
   logger "INFO: DONE executing $BENCH_SUITE"
@@ -24,5 +37,5 @@ benchmark_suite_run() {
 # $1 query number
 execute_query_spark() {
   local query="$1"
-  execute_spark "$query" "spark-sql -e \"USE $TPCH_DB_NAME; \$(< $D2F_local_dir/tpch/queries/$query.sql)\"" "time"
+  execute_spark-sql "$query" "-e \"USE $TPCH_DB_NAME; \$(< $D2F_local_dir/tpch/queries/$query.sql)\"" "time"
 }
