77b7047da5a2eef4792b6dc6ae76a5d2a4675c47
Alejandro <alejandro.montero.rivero@gmail.com>
2017-03-06 17:25:09 +0100

    BB new feature: Multiple parallel executions with different scale factors.

--- shell/common/benchmark_testParallelism.sh
+++ shell/common/benchmark_testParallelism.sh
@@ -5,12 +5,21 @@ benchmark_suite_run() {
   local bench_name="BB__${FUNCNAME[0]#benchmark_}-$1"
   local cmd=""
 
-  for thread in $(seq -f "%g" -s " "  1 5) ; do #Useful to go from 1 to n...
-      cmd+="echo I am thread $thread, STARTING now...
-    sleep 10 && echo I am thread $thread, STOPPING now... &
-    "
-    echo "CMD:
-    $cmd"
+  thread="1"
+  for stream in $(seq -f "%g" -s " "  1 1) ; do #Useful to go from 1 to n...
+
+      for query in $(seq -f "%g" -s " "  1 5) ; do #Useful to go from 1 to n...
+          cmd+="wait &
+          "
+          for scale_factor in $(seq -f "%g" -s " "  1 2) ; do #Useful to go from 1 to n...
+            local thread="$(printf %.$2f $(echo "$thread" | bc))"
+            cmd+="echo I am stream $stream running query $query, with scale factor $scale_factor, STARTING now...
+            sleep 10  && echo I am scale factor $thread1 in stream $thread2, starting query $thread3, STOPPING now... &
+            "
+            ((thread++))
+          done
+      done
+
   done
 
   cmd+="wait"
