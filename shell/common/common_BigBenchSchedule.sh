5235479def8a95272f50e30c4f91455ee2a16075
victorcuevasv <victorcuevasv@gmail.com>
2017-03-24 19:08:41 +0100

    Added an export in the generated script to change the java.io.tmpdir parameter.

--- shell/common/common_BigBenchSchedule.sh
+++ shell/common/common_BigBenchSchedule.sh
@@ -30,18 +30,18 @@ generateScheduleFile() {
         	workLoad=$((workLoad*$3))
         	printf "%s\n"  "$workLoad" >> "$2"
         	# Generate the schedule for each workload
-        	#shuffle
+        	shuffle
         	for ((i=1;i<=workLoad;i++)); do
             	#The two lines below enable to chose a query randomly,
             	#thus repetition can occur
             	#Add zero since positions in the array begin with 0
-            	queryIdx="$((0 + (RANDOM % queriesLen)))"
-            	query=${queries[queryIdx]}
+            	#queryIdx="$((0 + (RANDOM % queriesLen)))"
+            	#query=${queries[queryIdx]}
             	#The line below uses the fact that the queries array
             	#has been shuffled, thus avoiding the possible repetition
             	#of a query. However, there must be sufficient queries in
             	#order to fulfill the schedule.
-            	#query=${queries[i]}
+            	query=${queries[$((i-1))]}
             	printf "%s "  "$query" >> "$2"
         	done
         	printf "\n" >> "$2"
@@ -81,6 +81,7 @@ generateExecutionScript() {
 	local bb_exports
 	local bb_bin
 	local bb_cmd
+	local batch_exports
 	bb_exports="$(get_BigBench_exports  "$4")"
 	bb_bin="$(get_BigBench_cmd_schedule)"
 	printf "%s\n" "$bb_exports"  >> $2
@@ -95,12 +96,19 @@ generateExecutionScript() {
 	local i=0
 	#Obtain the number of lines in the schedule file to add only the necessary sleeps
 	local nLines=$(cat "$1" | wc -l)
+	# Enumerates the current batch in the loop, whereas nBatch is a variable within the generated script.
+	local batchCounter=0
  	#Read the file line by line
 	while IFS='' read -r line || [[ -n "$line" ]]; do
    		if [ $((lineIdx%2)) -eq 0 ]; then
        		nQueries="$line"
        		timePerQuery=$((secPerWorkload / nQueries))
        		printf "nBatch=\$((nBatch+1))\n" >> $2
+       		batchCounter=$((batchCounter+1))
+       		# Add the exports specific to the batch
+			batch_exports="$(get_BigBench_exports_batch  "$4" "$batchCounter")"
+			printf "%s\n" "$batch_exports"  >> $2
+			printf 
        		printf "echo \"---------------------WORKLOAD \$nBatch STARTED -------------------\" >> $3 \n" >> $2
        		printf "nQuery=1\n" >> $2
    		else
@@ -114,7 +122,7 @@ generateExecutionScript() {
            		query=${queries[i]}
            		printf "%s\n" "tNow=\$(date +\"%s\") ; " >> $2
            		printf "%s\n" "echo \"QUERY \$nQuery (Q$query) OF BATCH \$nBatch STARTED \$(((tNow-initTime))) AFTER\" >>$3 ;" >> $2
-           		bb_cmd="( $bb_bin runQuery -q $query -U -z ${BIG_BENCH_PARAMETERS_FILE}_$4 ; "
+           		bb_cmd="( $bb_bin runQuery -q $query -U -z ${BIG_BENCH_PARAMETERS_FILE}_$4 -t $batchCounter ; "
 				bb_cmd+="t=\$(date +\"%s\") ; "
 				bb_cmd+="echo \"QUERY \$nQuery (Q$query) OF BATCH \$nBatch COMPLETED \$(((t-initTime))) AFTER\" >> $3 ) &"
 				printf "%s\n" "$bb_cmd" >> $2
@@ -129,4 +137,20 @@ generateExecutionScript() {
 	done < "$1"
 	printf "wait\n" >> $2
 }
+
+# Helper to print a line with required exports for a particular batch
+# $1 scale factor to use
+# $2 batch number
+get_BigBench_exports_batch() {
+  local to_export
+  to_export="
+    export BIG_BENCH_LOGS_DIR='$(get_local_bench_path)/BigBench_logs/bigbench_$1/batch_$2';
+    export BIG_BENCH_HDFS_ABSOLUTE_QUERY_RESULT_DIR='$HDFS_DATA_ABSOLUTE_PATH/query_results/bigbench_$1/batch_$2';
+    export BIG_BENCH_HDFS_ABSOLUTE_TEMP_DIR='$HDFS_DATA_ABSOLUTE_PATH/bigbench_$1/batch_$2/temp';
+    #################################### IMPORTANT #################################################
+    export HADOOP_OPTS=' -Djava.io.tmpdir=./tmp ';"
+  echo -e "$to_export\n"
+}
+
+
 
