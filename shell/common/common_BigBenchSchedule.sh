12b19f95bc6f5305121781f2a19873e5d7305a46
victorcuevasv <victorcuevasv@gmail.com>
2017-10-19 15:00:30 +0200

    First commit, replaced schedule scripts.

--- shell/common/common_BigBenchSchedule.sh
+++ shell/common/common_BigBenchSchedule.sh
@@ -10,172 +10,6 @@ get_BigBench_cmd_schedule() {
   echo -e "$BigBench_cmd"
 }
 
-# $1 input workload file
-# $2 output schedule file
-# $3 batch multiplier
-# $4 random seed (-1 to do not set the seed)
-generateScheduleFile() {
-	# Set the queries to choose from
-	# This line enables all 30 queries (specified as a sequence)
-	queries=($(seq -f "%g" -s " "  1 30))
-	# This line enables only particular queries (e.g. only SQL queries)
-	# queries=(6 7 9 11 12 13 14 15 16 17 21 22 23 24)
-	queriesLen=${#queries[@]}
-	# IMPORTANT: setting the random seed guarantees the same schedule every time
-	if [ $4 -ne -1 ]; then
-		RANDOM=$4
-	fi
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-    	for word in $line; do 
-        	# NOTE: the following commented lines will work only for integer values
-			# Convert the floating point number read into an integer
-        	# IMPORTANT: the number is truncated
-        	# workLoad=${word%.*}
-        	# Scale the workload by the multiplier
-        	# workLoad=$((workLoad*$3))
-			# NOTE: the following lines work for integer and floating point values
-			# Scale the workload by the multiplier
-			workLoad=$(echo $word*$3 | bc)
-			# Round the number
-			workLoad=$(echo "($workLoad+0.5)/1" | bc)
-        	printf "%s\n"  "$workLoad" >> "$2"
-        	# Generate the schedule for each workload
-        	shuffle
-        	for ((i=1;i<=workLoad;i++)); do
-            	#The two lines below enable to chose a query randomly,
-            	#thus repetition can occur
-            	#Add zero since positions in the array begin with 0
-            	#queryIdx="$((0 + (RANDOM % queriesLen)))"
-            	#query=${queries[queryIdx]}
-            	#The line below uses the fact that the queries array
-            	#has been shuffled, thus avoiding the possible repetition
-            	#of a query. However, there must be sufficient queries in
-            	#order to fulfill the schedule.
-            	query=${queries[$((i-1))]}
-            	# Generate a matching scale factor for the query based on the fact
-				# that a given percentage must belong to that scale factor
-				scalesArray=(1 10)
-				probsArray=(0 0.999 1.0)
-				scalesLen=${#scalesArray[@]}
-				scale=0
-				# If the random number is obtained by RANDOM within the pipelined call to bc, the predefined seed
-				# is no longer effective, so we invoke it outside the bc call and store the value in a temp variable
-				probt=$RANDOM
-				prob=$(echo "scale=4 ; $probt/32767.0" | bc)
-				echo "prob: $prob"
-				for ((j=0; j<scalesLen; j++)); do
-    				if (( $(bc <<< "$prob >= ${probsArray[j]}") && $(bc <<< "$prob < ${probsArray[j+1]}") ))
-    				then
-        				scale=${scalesArray[j]}
-    				fi
-    			done
-            	printf "%s:%s "  "$query" "$scale" >> "$2"
-        	done
-        	printf "\n" >> "$2"
-    	done
-	done < "$1"	
-}
-
-#This function shuffles the elements of an array in-place using the 
-#Knuth-Fisher-Yates shuffle algorithm. 
-shuffle() {
-   local i tmp size max rand
-
-   # $RANDOM % (i+1) is biased because of the limited range of $RANDOM
-   # Compensate by using a range which is a multiple of the array size.
-   size=${#queries[*]}
-   max=$(( 32768 / size * size ))
-
-   for ((i=size-1; i>0; i--)); do
-      while (( (rand=$RANDOM) >= max )); do :; done
-      rand=$(( rand % (i+1) ))
-      tmp=${queries[i]} queries[i]=${queries[rand]} queries[rand]=$tmp
-   done
-}
-
-# $1 schedule file generated from the workload
-# $2 name of the output script to be generated
-# $3 name of the output log that the execution of the generated script will generate
-# $4 scale factor for the BigBench data
-# $5 time to wait (in seconds) between each batch
-generateExecutionScript() {
-	local lineIdx=0
-	local nQueries=0
-	local queries=()
-	local scaleFactors=()
-	local secPerWorkload=$5
-	printf "#!/bin/bash\n" >> "$2"
-	printf "#Script generated dynamically to execute the workload\n" >> $2
-	local bb_exports
-	local bb_bin
-	local bb_cmd
-	local query_exports
-	bb_exports="$(get_BigBench_exports  "$4")"
-	bb_bin="$(get_BigBench_cmd_schedule)"
-	printf "%s\n" "$bb_exports"  >> $2
-	printf "%s\n" "export CLASSPATH=\$CLASSPATH:\$SPARK_DIST_CLASSPATH"  >> $2
-	printf "echo \"####################### echoing CLASSPATH ################################\"\n" >> $2
-	printf "%s\n" "echo \$CLASSPATH" >> $2
-	printf "echo \"####################### echoing CLASSPATH ################################\"\n" >> $2
-	printf "\n" >> $2
-	printf "nBatch=0\n" >> $2
-	printf "nQuery=0\n" >> $2
-	printf "%s\n" "initTime=\$(date +\"%s\")" >> $2 
-	printf "t=0\n" >> $2 
-	printf "echo \"EXECUTION BEGINS AT: \$(date)\" >> $3 \n" >> $2
-	printf "echo \"initTime: \$initTime\" >> $3 \n" >> $2
-	printf "tNow=0\n" >> $2 
-	local i=0
-	#Obtain the number of lines in the schedule file to add only the necessary sleeps
-	local nLines=$(cat "$1" | wc -l)
-	# Enumerates the current batch in the loop, whereas nBatch is a variable within the generated script.
-	local batchCounter=0
- 	#Read the file line by line
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-   		if [ $((lineIdx%2)) -eq 0 ]; then
-       		nQueries="$line"
-       		timePerQuery=$((secPerWorkload / nQueries))
-       		printf "nBatch=\$((nBatch+1))\n" >> $2
-       		batchCounter=$((batchCounter+1))
-       		printf "echo \"---------------------WORKLOAD \$nBatch STARTED -------------------\" >> $3 \n" >> $2
-       		printf "nQuery=1\n" >> $2
-   		else
-       		idx=0
-       		#Tokenize the line read to separate the different numbers
-       		for word in $line; do 
-       			# The queries are read in the form query:scale_factor, so these two elements need to be separated
-				# by creating a two-element array with them
-				pairArray=(${word//:/ })
-           		word1="${pairArray[0]}"
-           		word2="${pairArray[1]}"
-           		queries[idx]="$word1"
-           		scaleFactors[idx]="$word2"
-           		idx=$((idx+1))
-       		done
-       		for ((i=0;i<nQueries;i++)); do
-           		query=${queries[i]}
-           		scaleFactor=${scaleFactors[i]}
-           		# Add the exports specific to the query
-				query_exports="$(get_BigBench_exports_query  "$scaleFactor" "$batchCounter")"
-				printf "%s\n" "$query_exports"  >> $2
-           		printf "%s\n" "tNow=\$(date +\"%s\") ; " >> $2
-           		printf "%s\n" "echo \"QUERY \$nQuery (Q$query) OF BATCH \$nBatch STARTED \$(((tNow-initTime))) AFTER\" >>$3 ;" >> $2
-           		bb_cmd="( $bb_bin runQuery -q $query -U -z ${BIG_BENCH_PARAMETERS_FILE}_$scaleFactor -t $batchCounter -f $scaleFactor ; "
-				bb_cmd+="t=\$(date +\"%s\") ; "
-				bb_cmd+="echo \"QUERY \$nQuery (Q$query) OF BATCH \$nBatch COMPLETED \$(((t-initTime))) AFTER\" >> $3 ) &"
-				printf "%s\n" "$bb_cmd" >> $2
-				printf "nQuery=\$((nQuery+1))\n" >> $2
-       		done
-       		#Add a sleep to mark the next batch (avoid it for the last batch)
-       		if [[ $((lineIdx+2)) -lt "$nLines" ]]; then
-       			printf "sleep %s\n" "$secPerWorkload" >> $2
-       		fi
-   		fi
-   		lineIdx=$((lineIdx+1))
-	done < "$1"
-	printf "wait\n" >> $2
-}
-
 # Helper to print a line with required exports for a particular query
 # $1 scale factor to use
 # $2 batch number
@@ -192,5 +26,84 @@ get_BigBench_exports_query() {
   echo -e "$to_export\n"
 }
 
+# $1 Supplied workload file
+# $2 Schedule file to be generated
+# $3 Log directory
+# $4 Batch multiplier
+# $5 Random seed
+# $6 Degree of parallelism
+# $7 Maximum number of queries
+# $8 Batch wait time
+# $9 Spread queries across batch
+# ${10} Thread pool size
+# ${11} Queries degree of parallelism table file (null to use default)
+# ${12} time (internal to aloja)
+# ${13} enable void batches
+execute_BigBench_schedule() {
+	local bench_name="${FUNCNAME[0]#benchmark_}"
+	logger "INFO: Running $bench_name"
 
+	local time_exec="$12"
+	echo "----------------------EXECUTING SCHEDULE--------------------------"
+	cat "$1"
+	echo $1
+	# Delete the old generated schedule file
+	rm "$2"
+	# mainExports sets JAVA_HOME, which is needed by the schedule generator
+	rm $mainExportsFile
+	mainExports="$(get_BigBench_exports  "$5")"
+	printf "%s\n" "$mainExports"  >> $mainExportsFile
+	# sourcing the script may not make available the environment variables within java
+	# if execute_master is used (more comments below)
+	source $mainExportsFile
+	# args[0] input workload file
+	# args[1] output schedule file
+	# args[2] logs dir
+	# args[3] batch multiplier
+	# args[4] random seed
+	# args[5] queries array
+	# args[6] scale factors array
+	# args[7] probabilities array
+	# args[8] default degree of parallelism
+	# args[9] queries degree of parallelism table file (null to use default)
+	# args[10] max queries
+	# args[11] enable void batches
+	# Generate the new schedule file
+	# Use -cp instead of -jar in order to run a class which is not the Main-Class of the jar.
+	javaGenCmd='$JAVA_HOME/bin/java -cp $driverJar io.bigdatabenchmark.v1.driver.ScheduleGenerator '
+	javaGenCmd+='$1 $2 $3 $4 $5 "$BB_QUERIES" "$BB_SCALE_FACTORS" "$BB_PROBABILITIES" $6 ${11} $7 ${13}'
+	echo $javaGenCmd
+	# Use eval because of the escaped quotes in the arrays
+	eval $javaGenCmd
+	cat "$2"
+	echo $2
+	echo "RUNNING JAVA ALOJABBDRIVER"
+	bbBinary="$(get_BigBench_cmd_schedule)"
+	localBenchPath="$(get_local_bench_path)"
+	javaCmd="$JAVA_HOME/bin/java -Dalojarepo.path=$ALOJA_REPO_PATH -Dbigbench.binary=$bbBinary "
+	javaCmd+="-Dmainexports.file=$mainExportsFile -Dhdfsdata.absolutepath=$HDFS_DATA_ABSOLUTE_PATH "
+	javaCmd+="-Dlocalbench.path=$localBenchPath "
+	javaCmd+="-Dbigbench.paramsfile=$BIG_BENCH_PARAMETERS_FILE -jar $driverJar "
+	# args[0] thread pool size
+	# args[1] wrapper for the engine
+	# args[2] schedule file
+	# args[3] log file directory
+	# args[4] batch wait time in seconds
+	# args[5] spread queries (true/false) spread evenly the queries within a batch and ignore internal batch delay
+	# args[6] maximum number of queries to execute for the run
+	javaCmd+="${10} AlojaHiveWrapper $2 $3 $8 $9 $7"
+	echo $javaCmd
+	# The following line executes the java command as a simple shell command
+	# $javaCmd
+	# Run the command and time it
+	# execute_master "$bench_name" "bash $3" "time"
+	# Apparently, from within java the environment variables exported by sourcing the mainExportsFile
+	# are not available when invoking System.getenv() if execute_master is used. Therefore in the java
+	# driver the scripts to execute individual queries must have all the exports.
+	execute_master "$bench_name" "$javaCmd" "$time_exec" "dont_save"
+  	# Stop metrics monitors and save bench (if needed)
+	if [ "$time_exec" ] ; then
+    	save_BigBench "$bench_name"
+	fi
+}
 
